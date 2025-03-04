fa94c7553a858a6bbf0ca2d2bfef93f177a2e91f
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-03-04 10:42:21 +0000

    add initial implementation of Animal-related classes and farm structure

---
+++ src/entities/Animal.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Animal {
+    private final AnimalType type;
+
+    public Animal(AnimalType type) {
+        this.type = type;
+    }
+
+    public AnimalType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return type.name();
+    }
+}
---
+++ src/entities/AnimalType.java
@@ -0,0 +1,5 @@
+package entities;
+
+public enum AnimalType {
+    PIG, COW, SHEEP, LLAMA, CHICKEN
+}
